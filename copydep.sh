180cca23de3d150403c59106fe027b12ef226692
nirekin <nirekin@hotmail.com>
2018-08-31 15:30:54 +0200

    Ad copydep.sh

---
+++ copydep.sh
@@ -0,0 +1,11 @@
+echo "APi copydep..."
+
+# Refresh "model"
+rm -rf ./vendor/github.com/lagoon-platform/model/*.go
+cp ../model/*.go  ./vendor/github.com/lagoon-platform/model/
+
+# Refresh "engine"
+rm -rf ./vendor/github.com/lagoon-platform/engine/*.go
+cp ../engine/*.go  ./vendor/github.com/lagoon-platform/engine/
+rm -rf ./vendor/github.com/lagoon-platform/engine/ssh/*.go
+cp ../engine/ssh/*.go  ./vendor/github.com/lagoon-platform/engine/ssh
